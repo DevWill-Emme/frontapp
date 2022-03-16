c9414296d8aed6c38923606348f0ec4de27da2a3
DevWill-Emme <mustliergarridojavier@gmail.com>
2022-03-16 10:17:17 +0100

    navigationBar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import Thunk from "redux-thunk";
+
 import "./index.css";
 import App from "./App";
 
+import { Rsidebar, Rpath } from "./Components/Navigation/reducers/reducers";
+//import { Rselect } from "./Components/Navigation/reducers/reducerSelect";
+
+const reducers = combineReducers({ Rsidebar, Rpath });
+const store = createStore(reducers, applyMiddleware(Thunk));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
